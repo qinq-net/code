6b28445b0aceb4dd05d75432b5699ab59afdd627
Qin Yuhao <qinq_net@buaa.edu.cn>
2016-09-23 15:09:56 +0800

    modified:   fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,3 +1,4 @@
+#!/usr/bin/env fish
 # vim: set noet:
 #
 # Set the default prompt command.
@@ -24,24 +25,53 @@ function fish_prompt --description "Write out the prompt"
 		set suffix '$'
 	end
 	
+
 	export __fish_git_prompt_showcolorhints=1
 	export __fish_git_prompt_show_informative_status=1
 	export fish_prompt_pwd_dir_length=0
 
-	echo -s (set_color $fish_color_operator) "╔═[" \
-		(set_color $fish_color_user) "$USER" \
-		(set_color $fish_color_autosuggestion) "@" \
-		(set_color $fish_color_host) "$__fish_prompt_hostname" \
-		(set_color $fish_color_operator) "] " \
-		(set_color normal) "-" \
-		(set_color $fish_color_operator) " [" \
-		(set_color $fish_color_quote) (date +"%a %b %d, %k:%M") \
-		(set_color $fish_color_operator) "]"
-	echo -s (set_color $fish_color_operator) "╚╦─[" \
-		(set_color $color_cwd) (prompt_pwd) \
-		(set_color $fish_color_operator) "] <" \
-		(set_color normal) (__fish_svn_prompt) \
-		(set_color normal) (__fish_git_prompt "(%s)") \
-		(set_color $fish_color_operator) ">"
+	if [ "$COLUMNS" -gt "60" ];
+		echo -s (set_color $fish_color_operator) "╔═[" \
+			(set_color $fish_color_user) "$USER" \
+			(set_color $fish_color_autosuggestion) "@" \
+			(set_color $fish_color_host) "$__fish_prompt_hostname" \
+			(set_color $fish_color_operator) "] " \
+			(set_color normal) "-" \
+			(set_color $fish_color_operator) " [" \
+			(set_color $fish_color_quote) (date +"%a %b %d, %k:%M") \
+			(set_color $fish_color_operator) "]"
+	else
+		echo -s (set_color $fish_color_operator) "╔═[" \
+			(set_color $fish_color_user) "$USER" \
+			(set_color $fish_color_autosuggestion) "@" \
+			(set_color $fish_color_host) "$__fish_prompt_hostname" \
+			(set_color $fish_color_operator) "]"
+	end
+	
+	
+	set prompt_versioncon 	(echo -sn 	(timeout 0.5s fish -c __fish_svn_prompt) \
+						(timeout 0.5s fish -c __fish_git_prompt "(%s)") )
+	export long_prompt_line2=( export fish_prompt_pwd_dir_length=0;
+				echo -s (set_color $fish_color_operator) "╚╦─[" \
+					(set_color $color_cwd) (prompt_pwd) \
+					(set_color $fish_color_operator) "] <" \
+					(set_color normal) "$prompt_versioncon" \
+					(set_color $fish_color_operator) ">" )
+	export short_prompt_line2=( export fish_prompt_pwd_dir_length=1;
+				echo -s (set_color $fish_color_operator) "╚╦─[" \
+					(set_color $color_cwd) (prompt_pwd) \
+					(set_color $fish_color_operator) "] <" \
+					(set_color normal) "$prompt_versioncon" \
+					(set_color $fish_color_operator) ">" )
+	if [ (expr length (echo -n "$long_prompt_line2"|sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")) -gt "$COLUMNS" ];
+		if [ (expr length (echo -n "$short_prompt_line2"|sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")) -gt "$COLUMNS" ];
+			echo -s (set_color $fish_color_operator) "╚╦─[...]"
+		else
+			echo "$short_prompt_line2"
+		end
+	else
+		echo "$long_prompt_line2"
+	end
+	
 	echo -n -s (set_color $fish_color_operator) " ╚═[" (set_color normal) "$suffix" (set_color $fish_color_operator) "] " (set_color normal) 
 end
