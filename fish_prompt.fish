db1fb922a26a43ea632658dd9fb5623cd48f07e9
Qin Yuhao <qinq_net@buaa.edu.cn>
2016-12-10 21:31:49 +0800

    add a space after each prompt line

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -68,13 +68,13 @@ function fish_prompt --description "Write out the prompt"
 			(set_color normal) "-" \
 			(set_color $fish_color_operator) " [" \
 			(set_color $fish_color_quote) (date +"%a %b %d, %k:%M") \
-			(set_color $fish_color_operator) "]"
+			(set_color $fish_color_operator) "] "
 	else
 		echo -s (set_color $fish_color_operator) "╔═[" \
 			(set_color $fish_color_user) "$USER" \
 			(set_color $fish_color_autosuggestion) "@" \
 			(set_color $fish_color_host) "$__fish_prompt_hostname" \
-			(set_color $fish_color_operator) "]"
+			(set_color $fish_color_operator) "] "
 	end
     echo -ns "." >&2
 	
@@ -84,20 +84,22 @@ function fish_prompt --description "Write out the prompt"
 					(set_color $color_cwd) (prompt_pwd) \
 					(set_color $fish_color_operator) "] <" \
 					(set_color normal) "$prompt_versioncon" \
-					(set_color $fish_color_operator) ">" )
+					(set_color $fish_color_operator) "> " )
 	export short_prompt_line2=( export fish_prompt_pwd_dir_length=1;
 				echo -s (set_color $fish_color_operator) "╚╦─[" \
 					(set_color $color_cwd) (prompt_pwd) \
 					(set_color $fish_color_operator) "] <" \
 					(set_color normal) "$prompt_versioncon" \
-					(set_color $fish_color_operator) ">" )
+					(set_color $fish_color_operator) "> " )
     echo -ns "." >&2
 
 	#if [ (expr length (echo -n "$long_prompt_line2"|sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")) -gt "$COLUMNS" ];
 	#	if [ (expr length (echo -n "$short_prompt_line2"|sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")) -gt "$COLUMNS" ];
 	if [ (sw "$long_prompt_line2") -gt "$COLUMNS" ];
 		if [ (sw "$short_prompt_line2") -gt "$COLUMNS" ];
-			echo -s (set_color $fish_color_operator) "╚╦─[...]"
+			echo -s (set_color $fish_color_operator) "╚╦─[...] <" \
+                    (set_color normal) "$prompt_versioncon" \
+                    (set_color $fish_color_operator) "> "
 		else
 			echo "$short_prompt_line2"
 		end
